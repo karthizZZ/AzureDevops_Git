4515e90dc86b03993cfbe0baeb719df7f1a6db93
Karthik CD <karthiz4u@outlook.com>
2023-01-28 01:37:31 +0530

    Add project files.

---
+++ AzureDevops_Git/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDevops_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
